bb7ebb220f6a015eb848c66b258b1d12975662b7
jigar makwana <jigarmakwana34065@gmail.com>
2025-08-08 17:26:31 +0530

    Add homepage and deploy scripts for GitHub Pages

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
+
+    const [isActive, setIsActive] = useState(false)
+
+    const handleToggle = () => {
+        setIsActive(!isActive)
+    }
+
+    const handleCloseMenu = () => {
+        setIsActive(false)
+    }
+
     return (
         <>
 
@@ -103,28 +114,75 @@ const Header = () => {
                                 </NavLink>
                             </div>
 
-                            <div className="menu">
+                            <div className={`menu ${isActive ? 'active' : ''}`}>
+
+                                <div className="logo">
+                                    <NavLink to={'/'}>
+                                        <img src="img/shivshakti.png" alt="" />
+                                    </NavLink>
+                                </div>
+
                                 <ul className='flex'>
                                     <li>
-                                        <NavLink to={'/'}>home</NavLink>
+                                        <NavLink to={'/'} onClick={handleCloseMenu}>home</NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to={'/about'}>about us</NavLink>
+                                        <NavLink to={'/about'} onClick={handleCloseMenu}>about us</NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to={'/products'}>products</NavLink>
+                                        <NavLink to={'/products'} onClick={handleCloseMenu}>products</NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to={'/process'}>process</NavLink>
+                                        <NavLink to={'/process'} onClick={handleCloseMenu}>process</NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to={'/nutrients'}>nutrients</NavLink>
+                                        <NavLink to={'/nutrients'} onClick={handleCloseMenu}>nutrients</NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to={'/contact'}>contact us</NavLink>
+                                        <NavLink to={'/contact'} onClick={handleCloseMenu}>contact us</NavLink>
                                     </li>
                                 </ul>
+
+                                <div className="social">
+                                    <h4>Follow us on:</h4>
+
+                                    <ul className='flex'>
+                                        <li>
+                                            <a href="">
+                                                <img src="../../img/youtube.png" alt="" />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="">
+                                                <img src="../../img/facebook.png" alt="" />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="">
+                                                <img src="../../img/instagram.png" alt="" />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="">
+                                                <img src="../../img/linkedin.png" alt="" />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="">
+                                                <img src="../../img/twitter.png" alt="" />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
+
+                            <label className='menubar'>
+                                <input type="checkbox" checked={isActive} onChange={handleToggle}></input>
+                                <svg viewBox="0 0 32 32">
+                                    <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+                                    <path class="line" d="M7 16 27 16"></path>
+                                </svg>
+                            </label>
                         </div>
                     </div>
                 </div>
