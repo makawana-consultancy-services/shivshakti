0ceb588faf132bd1124b3b55fd4b839b68e432c5
jigar makwana <jigarmakwana34065@gmail.com>
2025-08-08 19:02:49 +0530

    header issues solve

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -109,7 +109,7 @@ const Header = () => {
                     <div className="row">
                         <div className="main-box flex">
                             <div className="logo">
-                                <NavLink to={'/'}>
+                                <NavLink to={'/'} onClick={handleCloseMenu}>
                                     <img src="img/shivshakti.png" alt="" />
                                 </NavLink>
                             </div>
